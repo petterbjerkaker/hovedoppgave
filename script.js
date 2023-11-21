3d662efbaaa7226500259580f9891102e782d1e8
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2023-11-21 17:22:17 +0100

    Add new webpage function on button click

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const slideshow = document.querySelector(".slideshow");
+
+const newWebpageButton = document.querySelector(".new-website__registration");
+newWebpageButton.addEventListener("click", ()=>{
+	window.open(
+		"https://www.melhusil.no/next/p/18690/kontakt-oss",
+		"RegistrationMelhusMarathon2024",
+		
+	);
+});
