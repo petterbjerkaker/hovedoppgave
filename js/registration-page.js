42a9bb06807cc64248af5509ccc1b3b6ef527039
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2023-12-02 12:29:12 +0100

    Form inputs and radio inputs for success toast

--- js/registration-page.js
+++ js/registration-page.js
@@ -1,13 +1,37 @@
+const registrationButton = document.querySelector("#submit-registration-button");
+const errorMessage = document.querySelector(".error-message");
+const registrationInputs = document.querySelectorAll(".registration-input");
+const registrationToastContent = document.querySelector("#registration-toast-content")
+const registrationRadioInputs = document.querySelectorAll(".radio-input");
 
+registrationButton.addEventListener("click",()=>{
+	const isRadioEmpty = Array.from(registrationRadioInputs).every(
+		(input) => !input.checked
+	);
 
+	const isInputEmpty = Array.from(registrationInputs).some(
+		(input) => input.value.trim() === ""
+	);
 
+	if (isRadioEmpty || isInputEmpty) {
+		errorMessage.style.display = "block";
+	}else{
+		errorMessage.style.display = "none";
+		showRegistrationToast();
+	}
+});
 
-
-
+Array.from(registrationInputs).forEach((registrationInput)=>{
+	registrationInput.addEventListener("input", ()=>{
+		errorMessage.style.display = "none";
+	});
+});
 
 function showRegistrationToast(){
-	const registrationToastContent = document.querySelector("#toast-content");
 	registrationToastContent.className = "show";
 	setTimeout(function(){
-		registrationToastContent.className = registrationToastContent.className.replace("show", "");}, 6000);
-	}
+		registrationToastContent.className = registrationToastContent.className.replace("show", "");
+	}, 6000);
+}
+
+
